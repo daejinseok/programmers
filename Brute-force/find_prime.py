60715e4f76cc7c4d1bd78945895116551c11e639
Seok Daejin <daejin.seok@gmail.com>
2020-10-13 06:44:12 +0900

    auto

--- Brute-force/find_prime.py
+++ Brute-force/find_prime.py
@@ -1,17 +1,29 @@
+# 소수 찾기
+# https://programmers.co.kr/learn/courses/30/lessons/42839
+
 from itertools import permutations
+import math
+
+def is_prime(n):
+    if n < 2:
+        return 0
+    if n % 2 == 0 and n > 2: 
+        return 0
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return 0
+    return 1
 
 def solution(numbers):
 
-    nl = list(map(int, numbers))
-    nm = []
+    char_list = list(numbers)
+    char_perm = []
 
-    for i in range(1, len(nl) + 1):
-        nm = nm + list(permutations(nl, i))
+    for i in range(1, len(char_list) + 1):
+        char_perm = char_perm + list(permutations(char_list, i))
 
-    print(nm)
-    print(len(nm))
-
-
+    num_set = set(map(lambda n : int(''.join(n)), char_perm))
+    return sum(map(is_prime, num_set))
 
 
-solution("1234567")
+print(solution("17"))
