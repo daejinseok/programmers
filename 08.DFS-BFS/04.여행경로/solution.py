66a2852030d4325a8b5dd8e869a5ba750006effc
Seok Daejin <daejin.seok@gmail.com>
2020-10-17 15:51:56 +0900

    auto

---
+++ 08.DFS-BFS/04.여행경로/solution.py
@@ -0,0 +1,21 @@
+# 여행경로
+# https://programmers.co.kr/learn/courses/30/lessons/43164
+
+
+def solution(tickets):
+    """
+    >>> solution([['ICN', 'JFK'], ['HND', 'IAD'], ['JFK', 'HND']])
+    ['ICN', 'JFK', 'HND', 'IAD']
+    >>> solution([['ICN', 'SFO'], ['ICN', 'ATL'], ['SFO', 'ATL'], ['ATL', 'ICN'], ['ATL','SFO']])
+    ['ICN', 'ATL', 'ICN', 'SFO', 'ATL', 'SFO']
+    """
+
+    answer = []
+    return answer
+
+
+
+
+
+import doctest
+doctest.testmod()
