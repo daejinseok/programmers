b5e22ac73f491f12e0508fc1b66b32d5efd4b0e4
Seok Daejin <daejin.seok@gmail.com>
2020-10-17 09:10:46 +0900

    auto

--- 08.DFS-BFS/03.단어변환/solution.py
+++ 08.DFS-BFS/03.단어변환/solution.py
@@ -1,8 +1,5 @@
-import doctest
-
 # 단어 변환
 # https://programmers.co.kr/learn/courses/30/lessons/43163?language=python3
-
 
 def is_one_diff(a, b):
     """
@@ -13,23 +10,42 @@ def is_one_diff(a, b):
     >>> is_one_diff('bbb', 'abc')
     False
     """
-    diff = False
-
+    one_diff = False
     for i in range(len(a)):
         if a[i] != b[i]:
-            if diff:
+            if one_diff:
                 return False
-            diff = True
+            one_diff = True
+    return one_diff
 
-    return diff
-
-
+from queue import Queue
 def solution(begin, target, words):
-    answer = 0
-    return answer
+    """
+    >>> solution('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog'])
+    4
+    >>> solution('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log'])
+    0
+    """
 
+    if target not in words:
+        return 0
 
-print(solution('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog']))
-print(solution('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log']))
+    v = set()
+    q = Queue()
+    q.put((0, begin))
 
+    while(q):
+        d, w = q.get()
+        print(d, w)
+
+        if w == target:
+            return d
+
+        for w2 in words:
+            if is_one_diff(w, w2):
+                q.put((d+1, w2))
+                words.remove(w2)
+    return d
+
+import doctest
 doctest.testmod()
