0dddf438dfde857e078138eecb5134fa59fcb804
Seok Daejin <daejin.seok@gmail.com>
2020-10-21 18:57:29 +0900

    auto_office

--- 08.DFS-BFS/04.여행경로/solution.py
+++ 08.DFS-BFS/04.여행경로/solution.py
@@ -15,23 +15,47 @@ def solution(tickets):
 
     def _walk(used, remain):
 
+        if len(remain) == 0:
+            return used
+
+        here = used[len(used-1)][1]
+        ft = find_ticket(here, remain):
+
+        if len(ft) == 0:
+            return None
+
+        result = None
+
+        for i in ft:
+            used2 = set(used)
+            used2.add(i)
+            remain2 = set(remain)
+            remain2.remove(i)
+            result = _walk(used2, remain2) 
+        
+
+
+
+    def find_ticket(from_city, remain):
+        ft = []
+        for i in remain:
+            if ts[i][0] == from_city:
+                ft.push(i)
+        return ft
+
 
 
 
     for i in range(N):
         used = set(i)
-        remain = set(range(N))
-        remain.remove(i)
+        left = set(range(N))
+        left.remove(i)
         all = _walk(set(i), remain)
 
         if all != None :
             break
             
     return all
-
-
-
-
 
 import doctest
 doctest.testmod()
